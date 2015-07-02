a3a22c1c98fccfd03d415e1f28333b962e45096c
Hugo Freire <hfreire@exec.sh>
2015-07-02 20:31:13 +0200

    Added person database. Minor improvements.

---
+++ src/modules/database/person.js
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2015, Hugo Freire <hfreire@exec.sh>. All rights reserved.
+ */
+
+var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
+
+var path = __dirname + "/../../../db";
+var file = __dirname + "/../../../db/person.db";
+var db = new sqlite3.Database(file);
+
+function person() {
+    var moduleManager = {};
+}
+
+person.prototype.type = "DATABASE";
+
+person.prototype.name = "person";
+
+person.prototype.info = function() {
+    return "*" + this.name + "* - " +
+        "_" + this.name.charAt(0).toUpperCase() + this.name.slice(1) + " " +
+        this.type.toLowerCase() + " module_";
+}
+
+person.prototype.load = function(moduleManager) {
+    this.moduleManager = moduleManager;
+
+    this.moduleManager.on('database:person:setup', this._run);
+
+    this.moduleManager.on('database:person:create', this._run);
+    this.moduleManager.on('database:person:retrieve', this._get);
+    this.moduleManager.on('database:person:update', this._run);
+    this.moduleManager.on('database:person:delete', this._run);
+
+    if (!fs.existsSync(file)) {
+        fs.mkdirSync(path);
+    }
+}
+
+person.prototype._run = function(query, parameters, callback) {
+    var handler = function(error) {
+        if (error !== null) {
+            if (callback !== undefined) {
+                callback(error);
+            }
+        } else {
+            callback(null, this.lastID, this.changes);
+        }
+    };
+
+    if (parameters !== undefined) {
+        db.run(query, parameters, handler);
+    } else {
+        db.run(query, handler);
+    }
+}
+
+person.prototype._get = function(query, parameters, callback) {
+    var handler = function(error, row) {
+        if (callback !== undefined && error !== null) {
+            callback(error);
+        } else {
+            callback(null, row);
+        }
+    };
+
+    if (parameters !== undefined) {
+        db.get(query, parameters, handler);
+    } else {
+        db.get(query, handler);
+    }
+}
+
+person.prototype._all = function(query, parameters, callback) {
+    var handler = function(error, row) {
+        if (callback !== undefined && error !== null) {
+            callback(error);
+        } else {
+            callback(null, row);
+        }
+    };
+
+    if (parameters !== undefined) {
+        db.all(query, parameters, handler);
+    } else {
+        db.all(query, handler);
+    }
+}
+
+person.prototype.unload = function() {
+    db.close();
+}
+
+module.exports = new person();
