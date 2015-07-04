8f67f8083f0427a80e8d700b12dbbd0d560a2585
Hugo Freire <hfreire@exec.sh>
2015-07-04 23:26:55 +0200

    Removed skeletons. Added person notification module.

---
+++ src/modules/person/notification.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015, Hugo Freire <hfreire@exec.sh>. All rights reserved.
+ */
+
+function notification() {
+    var moduleManager = {};
+}
+
+notification.prototype.type = "PERSON";
+
+notification.prototype.name = "notification";
+
+notification.prototype.info = function () {
+    return "*" + this.name + "* - " +
+        "_" + this.name.charAt(0).toUpperCase() + this.name.slice(1) + " " +
+        this.type.toLowerCase() + " module_";
+};
+
+notification.prototype.load = function (moduleManager) {
+    this.moduleManager = moduleManager;
+
+    this.start();
+};
+
+notification.prototype.unload = function () {
+    this.stop();
+};
+
+notification.prototype.start = function () {
+
+    this.moduleManager.on('person:user:online', function (user) {
+        console.log(user.name + ' is online');
+    });
+
+    this.moduleManager.on('person:user:offline', function (user) {
+        console.log(user.name + ' is offline');
+    });
+};
+
+notification.prototype.stop = function () {
+};
+
+module.exports = new notification();
