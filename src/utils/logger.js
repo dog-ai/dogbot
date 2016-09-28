e36ed428e4beda9e6655ecb2edad934bb6a70019
Hugo Freire <hugo@dog.ai>
2016-09-28 22:17:19 +0200

    Updated logger code.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,29 +2,32 @@
  * Copyright (C) 2016, Hugo Freire <hugo@dog.ai>. All rights reserved.
  */
 
-var util = require('util'),
-  winston = require('winston'),
-  fs = require('fs'),
-  moment = require('moment');
+const LOG_TYPE = process.env.DOGBOT_LOG_TYPE || 'console'
+const LOG_LEVEL = process.env.DOGBOT_LOG_LEVEL || 'info'
+const ENVIRONMENT = process.env.DOGBOT_ENVIRONMENT || 'development'
+const BRANCH = process.env.DOGBOT_REPO_BRANCH || 'develop'
 
-var LOG_DIR = __dirname + '/../../var/log';
-var TMP_DIR = __dirname + '/../../var/tmp';
-var LOG_TYPE = process.env.DOGBOT_LOG_TYPE || 'console';
-var LOG_LEVEL = process.env.DOGBOT_LOG_LEVEL || 'info';
-var ENVIRONMENT = process.env.DOGBOT_ENVIRONMENT || 'development';
-var BRANCH = process.env.DOGBOT_REPO_BRANCH || 'develop';
+const util = require('util')
+const fs = require('fs')
+const path = require('path')
 
-winston.emitErrs = true;
+const winston = require('winston')
+const moment = require('moment')
+
+const LOG_DIR = path.join(__dirname, '/../../var/log')
+const TMP_DIR = path.join(__dirname, '/../../var/tmp')
+
+winston.emitErrs = true
 
 if (!fs.existsSync(LOG_DIR)) {
-  fs.mkdirSync(LOG_DIR);
+  fs.mkdirSync(LOG_DIR)
 }
 
-var timeFormat = function () {
-  return moment().format('YYYY-MM-DDTHH:mm:ss,SSSZ');
-};
+const timeFormat = () => {
+  return moment().format('YYYY-MM-DDTHH:mm:ss,SSSZ')
+}
 
-var transports = [];
+const transports = []
 
 switch (LOG_TYPE) {
   case 'console':
@@ -35,8 +38,8 @@ switch (LOG_TYPE) {
       timestamp: timeFormat,
       handleExceptions: true,
       humanReadableUnhandledException: true
-    }));
-    break;
+    }))
+    break
   case 'file':
     transports.push(new (winston.transports.File)({
       name: 'log', // http://stackoverflow.com/a/17374968
@@ -50,7 +53,7 @@ switch (LOG_TYPE) {
       timestamp: timeFormat,
       handleExceptions: true,
       humanReadableUnhandledException: true
-    }));
+    }))
 
     if (LOG_LEVEL === 'debug') {
       transports.push(new (winston.transports.File)({
@@ -65,58 +68,71 @@ switch (LOG_TYPE) {
         timestamp: timeFormat,
         handleExceptions: true,
         humanReadableUnhandledException: true
-      }));
+      }))
     }
-    break;
+    break
 }
 
 switch (ENVIRONMENT) {
   case 'production':
 
-    var rollbar = require('rollbar');
+    const rollbar = require('rollbar')
     rollbar.init('0e93b088a46f4376bc4c4d2fe871f832', {
       branch: BRANCH,
       environment: ENVIRONMENT
-    });
+    })
 
-    var RollbarLogger = function (options) {
-      this.name = 'rollbar';
-      this.level = options && options.level || 'error';
-      this.handleExceptions = true;
-      this.humanReadableUnhandledException = true;
-    };
+    const RollbarLogger = (options) => {
+      this.name = 'rollbar'
+      this.level = options && options.level || 'error'
+      this.handleExceptions = true
+      this.humanReadableUnhandledException = true
+    }
 
-    util.inherits(RollbarLogger, winston.Transport);
+    util.inherits(RollbarLogger, winston.Transport)
 
     RollbarLogger.prototype.log = function (level, msg, meta, callback) {
       if (level === 'error') {
-        var error = meta instanceof Error ? meta : msg;
+        let error
+        let payload = { level }
+        if (msg !== '' && meta) {
+          error = new Error()
+          error.stack = msg
 
-        rollbar.handleError(error, function (error) {
-          if (error) {
-            return callback(error);
-          } else {
-            return callback(null, true);
+          if (msg.indexOf('\n') > -1) {
+            error.message = msg.substring(7, msg.indexOf('\n'))
           }
-        });
+
+          payload.session = meta
+        } else {
+          error = meta
+        }
+
+        rollbar.handleErrorWithPayloadData(error, payload, function (error) {
+          if (error) {
+            return callback(error)
+          } else {
+            return callback(null, true)
+          }
+        })
       }
-    };
+    }
 
-    transports.push(new RollbarLogger());
+    transports.push(new RollbarLogger())
 
-    break;
+    break
 }
 
-var logger = new winston.Logger({
+const Logger = new winston.Logger({
   transports: transports,
   exitOnError: false
-});
+})
 
-module.exports = logger;
+module.exports = Logger
 
 module.exports.stream = {
-  write: function (message, encoding) {
-    logger.info(message);
+  write: function (message) {
+    Logger.info(message)
   }
-};
+}
 
