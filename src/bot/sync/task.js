9ad3858645182841171825a2b8ebfbc3cf317c7b
Hugo Freire <hugo@dog.ai>
2016-10-02 19:53:23 +0200

    Updated sync.

--- src/bot/sync/task.js
+++ src/bot/sync/task.js
@@ -2,45 +2,38 @@
  * Copyright (C) 2016, Hugo Freire <hugo@dog.ai>. All rights reserved.
  */
 
-var logger = require('../../utils/logger.js'),
-  Promise = require('bluebird');
+const Promise = require('bluebird')
+const Logger = require('../../utils/logger.js')
 
-var FirebaseQueue = require('firebase-queue');
+const FirebaseQueue = require('firebase-queue')
 
-function Task() {
-}
-
-Task.prototype.initialize = function (ref, companyId, onIncomingTaskCallback) {
-  var refs = {
-    tasksRef: ref.child('companies/' + companyId + '/tasks'),
-    specsRef: ref.child('queue/specs')
-  };
-
-  var options = {
-    'specId': 'default_spec',
-    'numWorkers': 1,
-    'suppressStack': true
-  };
-
-  this._queue = new FirebaseQueue(refs, options, function (task, progress, resolve, reject) {
-    logger.debug('Incoming task: ' + JSON.stringify(task));
-
-    if (!task || !task.event) {
-      return reject('Invalid task');
+class Task {
+  initialize (ref, companyId, onIncomingTaskCallback) {
+    const refs = {
+      tasksRef: ref.child('companies/' + companyId + '/tasks'),
+      specsRef: ref.child('queue/specs')
     }
 
-    onIncomingTaskCallback(task.event, task.data, progress, resolve, reject);
-  });
-};
+    const options = { specId: 'default_spec', numWorkers: 1, suppressStack: true }
 
-Task.prototype.terminate = function () {
-  var _this = this;
+    this._queue = new FirebaseQueue(refs, options, (task, progress, resolve, reject) => {
+      Logger.debug('Incoming task: ' + JSON.stringify(task))
 
-  return new Promise(function (resolve, reject) {
-    _this._queue.shutdown()
-      .then(resolve)
-      .catch(reject);
-  });
-};
+      if (!task || !task.event) {
+        return reject('Invalid task')
+      }
 
-module.exports = new Task();
+      onIncomingTaskCallback(task.event, task.data, progress, resolve, reject)
+    })
+  }
+
+  terminate () {
+    return new Promise((resolve, reject) => {
+      this._queue.shutdown()
+        .then(resolve)
+        .catch(reject)
+    })
+  }
+}
+
+module.exports = new Task()
