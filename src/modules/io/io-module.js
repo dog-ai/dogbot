f533a7c26ca4f2f979e71e7767146159cfe7914f
Hugo Freire <hugo@dog.ai>
2016-10-09 22:49:16 +0200

    Refactored slack io module.

--- src/modules/io/io-module.js
+++ src/modules/io/io-module.js
@@ -4,9 +4,37 @@
 
 const Module = require('../module')
 
+const UnknownIntentError = require('../nlp/errors/unknown-intent-error')
+
+const Logger = require('../../utils/logger')
+const Communication = require('../../utils/communication')
+
 class IOModule extends Module {
   constructor (name) {
     super('io', name)
+  }
+
+  onTextMessage (text) {
+    return new Promise((resolve, reject) => {
+      Communication.emitAsync('nlp:intent:text', text)
+        .timeout(5000)
+        .then(({ event, params }) => {
+          if (params.text) {
+            return resolve(params.text)
+          }
+
+          return Communication.emitAsync(event, params)
+            .then((text) => resolve(text))
+        })
+        .catch(UnknownIntentError, () => {
+          resolve('Oops')
+        })
+        .catch((error) => {
+          Logger.error(error)
+
+          reject(error)
+        })
+    })
   }
 }
 
