ef217fa046219556bc4710c76fb1097e82df587f
Hugo Freire <hugo@dog.ai>
2017-02-23 20:56:51 +0100

    fix(presence): sync presence performance

--- src/modules/performance/presence.js
+++ src/modules/performance/presence.js
@@ -4,7 +4,36 @@
 
 const PerformanceModule = require('./performance-module')
 
+const _ = require('lodash')
+
 const Bot = require('../../bot')
+
+const moment = require('moment')
+
+const createPresence = (presence) => {
+  if (presence.created_date !== undefined && presence.created_date !== null) {
+    presence.created_date = presence.created_date.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+  }
+
+  const keys = _.keys(presence);
+  const values = _.values(presence);
+
+  return Bot.emitAsync('database:performance:create',
+    'INSERT INTO presence (' + keys + ') VALUES (' + values.map(() => '?') + ');', values)
+    .catch((error) => {})
+}
+
+const findLatestPresenceByEmployeeId = (id) => {
+  return Bot.emitAsync('database:performance:retrieveOne',
+    'SELECT * from presence WHERE employee_id = ? ORDER BY created_date DESC;', [ id ])
+    .then(function (row) {
+      if (row) {
+        row.created_date = new Date(row.created_date.replace(' ', 'T'))
+      }
+
+      return row;
+    })
+}
 
 const enqueueStatsUpdateTask = (params, callback) => {
   const companyId = Bot.getCompanyId()
@@ -18,16 +47,67 @@ const enqueueStatsUpdateTask = (params, callback) => {
     .catch((error) => callback(error))
 }
 
+const addPresence = (employee) => {
+  findLatestPresenceByEmployeeId(employee.id)
+    .then((performance) => {
+      if (performance) {
+        if (performance.is_present == employee.is_present) {
+          return;
+        }
+
+        if (moment(employee.last_presence_date).isSame(moment(performance.created_date))) {
+          employee.last_presence_date = moment(employee.last_presence_date).add(1, 'second').toDate()
+        }
+      }
+
+      return createPresence({
+        employee_id: employee.id,
+        is_present: employee.is_present,
+        created_date: employee.last_presence_date
+      })
+    })
+};
+
+const onOutgoingPresenceSynchronization = (params, callback) => {
+  Bot.emit('database:performance:retrieveOneByOne',
+    'SELECT * FROM presence WHERE is_synced = 0', [], (error, row) => {
+      if (!error) {
+        if (row !== undefined) {
+          row.created_date = new Date(row.created_date.replace(' ', 'T'))
+          row.is_present = row.is_present == 1
+          row.name = 'presence'
+
+          callback(null, row, function (error) {
+            if (!error) {
+              Bot.emit('database:performance:update',
+                'UPDATE presence SET is_synced = 1 WHERE id = ?', [ row.id ])
+            }
+          })
+        }
+      }
+    })
+}
+
 class Presence extends PerformanceModule {
   constructor () {
     super('presence')
   }
 
   start () {
-    super.start({ 'performance:presence:stats:update': enqueueStatsUpdateTask })
+    super.start({
+      'performance:presence:stats:update': enqueueStatsUpdateTask,
+      'person:employee:nearby': addPresence,
+      'person:employee:faraway': addPresence,
+      'sync:outgoing:performance:presence': onOutgoingPresenceSynchronization
+    })
 
     const options = { schedule: '6 hours' }
     Bot.enqueueJob('performance:presence:stats:update', null, options)
+
+    Bot.emitAsync('sync:outgoing:periodic:register', {
+      companyResource: 'employee_performances',
+      event: 'sync:outgoing:performance:presence'
+    })
   }
 
   stop () {
